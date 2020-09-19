63820227ba8d9044baa5e11e084d9d5efa009cdf
Fodor Attila <atti@fodyweb.hu>
2020-09-19 16:21:19 +0200

    Add files via upload

--- download.bat
+++ download.bat
@@ -1,8 +1,8 @@
 set /P _link=YouTube link: 
 set _now=%date:~0,4%%date:~6,2%%date:~10,2%
 
-if not exist "%HOMEPATH%\Music\%_now%" mkdir "%HOMEPATH%\Music\%_now%"
+if not exist "%HOMEPATH%\Videos\%_now%" mkdir "%HOMEPATH%\Videos\%_now%"
 
-youtube-dl.exe "%_link%" -x --audio-format mp3 --audio-quality 320k --ffmpeg-location ffmpeg.exe -o "~/Music/%_now%/%%(title)s.%%(ext)s"
+youtube-dl.exe "%_link%"--ffmpeg-location ffmpeg.exe -o "~/Videos/%_now%/%%(title)s.%%(ext)s"
 
-start %HOMEPATH%\Music\%_now%
+start %HOMEPATH%\Videos\%_now%
